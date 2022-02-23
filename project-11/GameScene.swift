3d2a33671457d3d4b766d7fa875dee3ccd073339
brGuirra <br.guirra@gmail.com>
2022-02-22 22:50:02 -0300

    feat: display the game score to user

--- project-11/GameScene.swift
+++ project-11/GameScene.swift
@@ -9,6 +9,14 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         // Configure background
         let background = SKSpriteNode(imageNamed: "background")
@@ -17,6 +25,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         background.zPosition = -1
         addChild(background)
         
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
@@ -86,8 +99,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func colision(between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
     
